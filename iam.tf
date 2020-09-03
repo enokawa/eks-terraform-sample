0b8859953a2cc581bbd60560822cdf1d1924a869
enokawa <enokawa00@gmail.com>
2020-09-03 21:46:39 +0900

    Add IAM Roles for Service Accounts for ALB Ingress Controller

--- iam.tf
+++ iam.tf
@@ -60,3 +60,51 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly_at
   role       = aws_iam_role.eks_worker_node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+data "external" "thumbprint" {
+  program = ["sh", "shell/thumbprint.sh"]
+}
+
+
+resource "aws_iam_openid_connect_provider" "app" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.external.thumbprint.result.thumbprint]
+  url             = aws_eks_cluster.app.identity[0].oidc[0].issuer
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "alb_ingress_controller_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.app.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:alb-ingress-controller"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.app.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "alb_ingress_controller" {
+  name               = "${var.stage}-alb-ingress-controller-role"
+  assume_role_policy = data.aws_iam_policy_document.alb_ingress_controller_role_policy.json
+}
+
+resource "aws_iam_policy" "alb_ingress_controller" {
+  name        = "${var.stage}-alb-ingress-controller-policy"
+  path        = "/"
+
+  policy = file("policy/alb-ingress-controller.json")
+}
+
+resource "aws_iam_role_policy_attachment" "alb_ingress_controller_attach" {
+  role       = aws_iam_role.alb_ingress_controller.name
+  policy_arn = aws_iam_policy.alb_ingress_controller.arn
+}
